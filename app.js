63ab595a9849177f2f34357bcaaa7f3bec9147fe
Aigerim-Omirzak <74128426+Aigerim-Omirzak@users.noreply.github.com>
2024-02-20 01:13:34 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
+const PORT = 4000;
+
+app.get('/send-email', (req, res)=>{
+    res.render("contact");
+});
+
+app.post('/send-email', async (req, res)=>{
+    let {email, subject, text} = req.body;
+    try{
+        let errors = [];
+        if(!email || !subject || !text){
+            errors.push({message: "Please enter all fields"})
+        }
+        if(errors.length != 0){
+            console.log(errors)
+            res.render("contact", {errors});
+        } else {
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+				tls: {
+					rejectUnauthorized: false
+				},
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.EMAIL_PASS
+                }
+            });
+            try{
+                let info = await transporter.sendMail({
+                    from: process.env.EMAIL_USER,
+                    to: email,
+                    subject: subject,
+                    text: text
+                });
+                console.log('Message sent: %s', info.messageId);
+                res.send('Email send successfully');
+            }catch(error){
+                console.log(error);
+                res.send('Failed to send email');
+            }
+        }
+    }catch(error){
+        console.log(error);
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log("Server runs at port " + PORT);
+});
